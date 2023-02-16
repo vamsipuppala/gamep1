9b0919e623dcb9e9429df51a634d6f67221cf191
Anashwara16 <anashwara.rajinkumar@gmail.com>
2023-02-15 23:09:32 -0800

    Levels-1and2

---
+++ Assets/LevelTwoScripts/MainScreenScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScreenScript : MonoBehaviour
+{
+
+    public NextLevelScript nextLevelScript;
+
+
+    void Start()
+    {
+        //  loadMainScreen();
+       // nextLevelScript = GameObject.FindGameObjectWithTag("NextLevelManager").GetComponent<NextLevelScript>();
+
+    }
+    public void loadTutorialScene()
+    {
+        //nextLevelScript.resetValues();
+        SceneManager.LoadScene("LevelScenes/Tutorial_Level1");
+    }
+
+    public void loadLevelOneScene()
+    {
+        //nextLevelScript.resetValues();
+        SceneManager.LoadScene("LevelScenes/LevelOne");
+    }
+
+    public void loadMainScreen()
+    {
+        //nextLevelScript.resetValues();
+        SceneManager.LoadScene("LevelScenes/MainScreen");
+    }
+}
