26f72d1624bdd2d68ce6a51f0d2123206f6977ba
Gurpreet Kaur Saimy <gurpreet.saimy@gmail.com>
2023-03-05 22:11:33 -0800

    update threshold score

--- Assets/LevelThreeScripts/NextLevelThree.cs
+++ Assets/LevelThreeScripts/NextLevelThree.cs
@@ -12,7 +12,7 @@ public class NextLevelThree : MonoBehaviour
     //private int nextSceneToLoad;
     private bool loadScene = false;
     [SerializeField] private TextMeshProUGUI targetScore;
-    public int thresholdScoree = 5;
+    public int thresholdScoree = 6;
     public SendToGoogle sc;
     public PlayerControllerThree pc;
 
