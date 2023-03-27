ec7c47e71c42bdbdd25b7682080bd515df51165c
chanz1996 <chandiniglitter@gmail.com>
2023-03-26 20:45:30 -0700

    Initial UI Changes
    Modifications (Tutorial Round)
    - Tile textures
    - Clearer Letters
    - Background
    - Hint Popups
    - Positioning of Textual Elements

--- Assets/BlockSpawnerTutorialScript.cs
+++ Assets/BlockSpawnerTutorialScript.cs
@@ -71,6 +71,9 @@ public class BlockSpawnerTutorialScript : MonoBehaviour
                 GameObject block = Instantiate(blockPrefab, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
                 // block.transform.position = new Vector3(transform.position.x + (i * width) + (i * offset), posy, 0);
                 block.transform.position = new Vector3(transform.position.x + (i * width * blockScale) + (i * offset * blockScale), posy, 0);
+                block.GetComponentInChildren<TextMesh>().fontSize = 18; //added chan
+                block.GetComponentInChildren<TextMesh>().characterSize = 0.5F;
+                block.GetComponentInChildren<TextMesh>().alignment = TextAlignment.Right;
                 block.GetComponentInChildren<TextMesh>().text = Char.ToString(shuffledString[i]);
                 blocks[i] = block;
 
