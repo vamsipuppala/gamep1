f7fc4c34e04ae055c41f6628b56700f74054198e
Gurpreet Kaur Saimy <gurpreet.saimy@gmail.com>
2023-04-09 15:10:08 -0700

    level 8 ui

--- Assets/LevelFourScripts/L8_PlayerController.cs
+++ Assets/LevelFourScripts/L8_PlayerController.cs
@@ -72,6 +72,55 @@ public class L8_PlayerController : MonoBehaviour
     //record the frequency for each colored letter in target word
     Dictionary<char, int> targetColoredLetterFrequency;
 
+    static string greenHexCode = "#56a500";
+    Color greenColor = new Color(
+        (float)System.Convert.ToInt32(greenHexCode.Substring(1, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(greenHexCode.Substring(3, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(greenHexCode.Substring(5, 2), 16) / 255f,
+        1f
+    );
+
+    static string yellowHexCode = "#ecbd00";
+    Color yellowColor = new Color(
+        (float)System.Convert.ToInt32(yellowHexCode.Substring(1, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(yellowHexCode.Substring(3, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(yellowHexCode.Substring(5, 2), 16) / 255f,
+        1f
+    );
+
+    static string redHexCode = "#b90200";
+    Color redColor = new Color(
+        (float)System.Convert.ToInt32(redHexCode.Substring(1, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(redHexCode.Substring(3, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(redHexCode.Substring(5, 2), 16) / 255f,
+        1f
+    );
+
+    static string grayHexCode = "#69675E";
+    Color grayColor = new Color(
+        (float)System.Convert.ToInt32(grayHexCode.Substring(1, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(grayHexCode.Substring(3, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(grayHexCode.Substring(5, 2), 16) / 255f,
+        1f
+    );
+
+
+    static string obstacleDisableColorHexCode = "#635133";
+    public Color obstacleDisableColor = new Color(
+        (float)System.Convert.ToInt32(obstacleDisableColorHexCode.Substring(1, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(obstacleDisableColorHexCode.Substring(3, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(obstacleDisableColorHexCode.Substring(5, 2), 16) / 255f,
+        130f
+    );
+
+    static string obstacleOriginalColorHexCode = "#2D2020";
+    public Color obstacleOriginalColor = new Color(
+        (float)System.Convert.ToInt32(obstacleOriginalColorHexCode.Substring(1, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(obstacleOriginalColorHexCode.Substring(3, 2), 16) / 255f,
+        (float)System.Convert.ToInt32(obstacleOriginalColorHexCode.Substring(5, 2), 16) / 255f,
+        130f
+    );
+
     int ind = 0;
     void Start()
     {
@@ -381,8 +430,8 @@ public class L8_PlayerController : MonoBehaviour
                         else if (j == GetIndexOfGameObject(gameObject, nestedList))
                         {
 
-                            if (gameObject.GetComponent<SpriteRenderer>().color == Color.gray || gameObject.GetComponent<SpriteRenderer>().color == Color.red || gameObject.GetComponent<SpriteRenderer>().color == Color.green
-                                || gameObject.GetComponent<SpriteRenderer>().color == Color.yellow)
+                            if (gameObject.GetComponent<SpriteRenderer>().color == grayColor || gameObject.GetComponent<SpriteRenderer>().color == redColor || gameObject.GetComponent<SpriteRenderer>().color == greenColor
+                                || gameObject.GetComponent<SpriteRenderer>().color == yellowColor)
                             {
                                 localHits--;
                                 // numberOfTimeDeselectionsOccurred++;
@@ -439,7 +488,7 @@ public class L8_PlayerController : MonoBehaviour
 
                                         if (givenDangerWord[z1].Contains(text.text.ToString()))
                                         {
-                                            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+                                            gameObject.GetComponent<SpriteRenderer>().color = redColor;
                                             // dangerWordCreated += text.text;
                                             fla++;
                                             break;
@@ -448,7 +497,7 @@ public class L8_PlayerController : MonoBehaviour
                                     }
 
                                     if (!givenWord.Contains(text.text.ToString()) && fla == 0)
-                                        gameObject.GetComponent<SpriteRenderer>().color = Color.gray;
+                                        gameObject.GetComponent<SpriteRenderer>().color = grayColor;
 
                                     else if (givenWord.Contains(text.text.ToString()))
                                     {
@@ -456,9 +505,9 @@ public class L8_PlayerController : MonoBehaviour
 
                                         
                                         if (fla > 0)
-                                            gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+                                            gameObject.GetComponent<SpriteRenderer>().color = yellowColor;
                                         else
-                                            gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+                                            gameObject.GetComponent<SpriteRenderer>().color = greenColor;
                                         //givenWord = givenWord.Replace(text.text.ToString(), String.Empty);
 
                                         /* if (wordCreated.Length != bs.words[j].Length && goodword.text.IndexOf(wordCreated)!=-1)
@@ -526,8 +575,8 @@ public class L8_PlayerController : MonoBehaviour
                                             }
                                         }
                                         dest = true;
-                                        obstacle1.GetComponent<SpriteRenderer>().color = Color.red;
-                                        obstacle2.GetComponent<SpriteRenderer>().color = Color.red;
+                                        obstacle1.GetComponent<SpriteRenderer>().color = obstacleDisableColor;
+                                        obstacle2.GetComponent<SpriteRenderer>().color = obstacleDisableColor;
                                         boxCollider1.enabled = false;
                                         boxCollider2.enabled = false;
                                         StartCoroutine(EnableBox(15.0F));
@@ -651,8 +700,8 @@ public class L8_PlayerController : MonoBehaviour
     IEnumerator EnableBox(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        obstacle1.GetComponent<SpriteRenderer>().color = Color.white;
-        obstacle2.GetComponent<SpriteRenderer>().color = Color.white;
+        obstacle1.GetComponent<SpriteRenderer>().color = obstacleOriginalColor;
+        obstacle2.GetComponent<SpriteRenderer>().color = obstacleOriginalColor;
         boxCollider1.enabled = true;
         boxCollider2.enabled = true;
     }
